6acd907c69f751ad345e72176e2414fb1dccb237
RadhaRaquel <68845719+RadhaRaquel@users.noreply.github.com>
2021-06-04 19:42:02 +0200

    Delete Lesson 9_ Challenge directory

--- Lesson 9_ Challenge/addition.js
+++
@@ -1,7 +0,0 @@
-function Add() {
-    var x = document.getElementById("x");
-    var y = document.getElementById("y");
-    var sum = document.getElementById("answer");
-    var z = parseInt(x.value) + parseInt(y.value);
-    sum.innerText = z;
-}         
